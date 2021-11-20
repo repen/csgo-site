a47515b1691db4cb97cd76b6736964d1176a5b74
repen <9keepa@gmail.com>
2021-11-20 16:43:08 +0500

    install undetected_chromedriver

--- selenium/app/main.py
+++ selenium/app/main.py
@@ -18,47 +18,30 @@ import os, time
 from Globals import LOGIN, PASSWORD, SITE, SHARE_DIR
 from itertools import count
 from Model import HtmlData, ResultPage
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+# from selenium import webdriver
+# from selenium.webdriver.chrome.options import Options
 from tools import log
-log_content  = log("MAIN")
+
+import undetected_chromedriver.v2 as uc
+
+options = uc.ChromeOptions()
+
+# setting profile
+options.user_data_dir = "/tmp"
+
+# another way to set profile is the below (which takes precedence if both variants are used
+options.add_argument('--user-data-dir=/tmp')
+options.add_argument('--headless')
+
+# just some options passing in to skip annoying popups
+options.add_argument('--no-first-run --no-service-autorun --password-store=basic')
+driver = uc.Chrome(options=options)
+driver.maximize_window()
+
+log_content = log("MAIN")
 URL = SITE
 
-log_content.info( "Start script" )
-
-def init_driver():
-    co = Options()
-    # co.add_argument('--headless')
-    # co.add_argument('--no-sandbox')
-    co.add_argument('--disable-dev-shm-usage')
-    co.add_argument("--start-maximized")
-    co.add_argument("--disable-blink-features=AutomationControlled")
-    # co.add_argument("--start-fullscreen")
-    # disable infobars
-    co.add_argument('--disable-infobars')
-
-
-    # co.add_experimental_option("excludeSwitches", ["ignore-certificate-errors"])
-    co.add_experimental_option("excludeSwitches", ["enable-automation"])
-    co.add_experimental_option('useAutomationExtension', False)
-    chrome_prefs = {}
-    # chrome_prefs["profile.default_content_settings"] = {"images": 2}
-    # chrome_prefs["profile.managed_default_content_settings"] = {"images": 2}
-    co.add_experimental_option("prefs", chrome_prefs)
-    driver = webdriver.Chrome(
-        os.path.join( os.getcwd(), "chromedriver"), options=co)
-    return driver
-
-def init_driver_firefox():
-    option = webdriver.FirefoxOptions()
-    option.add_argument('-profile')
-    option.add_argument('/home/andreydev/.mozilla/firefox/x6pmbecc.default-release')
-    option.set_preference('dom.webdriver.enabled', False)
-    option.set_preference('dom.webnotifications.enabled', False)
-    driver = webdriver.Firefox(executable_path="./geckodriver", options=option,
-                               service_args=['--marionette-port', '2828'])
-
-    return driver
+log_content.info("Start script")
 
 
 def scene(driver):
@@ -67,29 +50,31 @@ def scene(driver):
     login = driver.find_element_by_css_selector("input.textField[name=username]")
     password = driver.find_element_by_css_selector("input.textField[name=password]")
     btn = driver.find_element_by_css_selector("#login_btn_signin input.btn_green_white_innerfade")
-    login.send_keys( LOGIN )
-    password.send_keys( PASSWORD )
+    login.send_keys(LOGIN)
+    password.send_keys(PASSWORD)
     btn.click()
     time.sleep(5)
+
 
 def work(driver, url):
     log_content.debug("Get %s", url)
     driver.get(url)
     time.sleep(8)
-    html = str( driver.page_source )
+    html = str(driver.page_source)
 
-    log_content.debug("Response %s. Length: %d", url, len( html ) )
+    log_content.debug("Response %s. Length: %d", url, len(html))
     log_content.debug("Write to db")
 
     HtmlData.insert_remove({
-        "m_time" : datetime.now().timestamp(),
-        "html" : html
-    } )
+        "m_time": datetime.now().timestamp(),
+        "html": html
+    })
 
     log_content.debug("End Job")
 
+
 def work_for_result_page(driver, m_id):
-    url = "https://betscsgo.in/match/{}".format( m_id )
+    url = "https://betscsgo.in/match/{}".format(m_id)
     log_content.debug("Get %s", url)
     driver.get(url)
     time.sleep(7)
@@ -101,64 +86,70 @@ def work_for_result_page(driver, m_id):
     ResultPage.insert({
         "m_time": datetime.now().timestamp(),
         "data": html,
-        "m_id" : m_id
+        "m_id": m_id
     }).execute()
 
     log_content.debug("End Job")
 
+
 def check_task():
     cache = set()
+
     def _check_task():
-        path = os.path.join( SHARE_DIR, "task.txt" )
+        path = os.path.join(SHARE_DIR, "task.txt")
         with open(path, "r", encoding="utf-8") as f:
             _data = f.read().strip()
-        current_ids = set( int(x) for x in _data.split("\n") if x)
+        current_ids = set(int(x) for x in _data.split("\n") if x)
         new_ids = current_ids.difference(cache)
         for el in new_ids:
-            cache.add( el )
-        log_content.info("New m_id: %s", str( new_ids ) )
+            cache.add(el)
+        log_content.info("New m_id: %s", str(new_ids))
         return new_ids
+
     return _check_task
+
 
 def prepare(driver):
     # driver = webdriver.Chrome()
     driver.execute_script("window.open('https://betscsgo.in/','_blank');")
     time.sleep(10)
-    driver.switch_to.window( driver.window_handles[0] )
+    driver.switch_to.window(driver.window_handles[0])
     driver.close()
-    driver.switch_to.window( driver.window_handles[0] )
+    driver.switch_to.window(driver.window_handles[0])
+
 
 def main():
     # driver = init_driver_firefox()
-    driver = init_driver()
+    # driver = init_driver()
 
-    log_content.debug("starting chrome")
-    log_content.debug("open page {}".format(URL))
-    driver.get(URL)
-    log_content.info("Waiting 15 sec")
-    
-    time.sleep(15)
-    
-    log_content.info("prepare browser")
-    prepare(driver)
-    log_content.debug("starting auth scene")
-    scene(driver)
+    with driver:
+        driver.get('https://betscsgo.in')  # known url using cloudflare's "under attack mode"
 
-    driver.get( URL )
-    log_content.debug("Checked length page:  {}".format( len( driver.page_source ) ))
+        log_content.debug("starting chrome")
+        log_content.debug("open page {}".format(URL))
+        driver.get(URL)
+        log_content.info("Waiting 15 sec")
 
-    _check_task = check_task()
+        time.sleep(15)
 
-    # =========== work position =============
-    for _ in count():
-        work(driver, URL)
+        scene(driver)
 
-        result = _check_task()
-        if result:
-            for m_id in result:
-                work_for_result_page(driver, m_id)
+        driver.get(URL)
+        log_content.debug("Checked length page:  {}".format(len(driver.page_source)))
 
-        time.sleep(25)
+        _check_task = check_task()
+
+        # =========== work position =============
+        for _ in count():
+            work(driver, URL)
+
+            result = _check_task()
+            if result:
+                for m_id in result:
+                    work_for_result_page(driver, m_id)
+
+            time.sleep(25)
+
 
 if __name__ == '__main__':
     main()
